eb3a835358bd53e758daf0438f00eb583dc77e13
thihaaung22 <thihaaung7779@gmail.com>
2019-04-02 09:09:03 -0700

    Fourth Commit Event Emitter Added

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const Person = require("./person");
+const Logger = require("./logger");
+
+const logger = new Logger();
+
+//need to on the EventEmitter class to use
+logger.on("message", data => console.log("Called Listener: ", data));
+logger.log("Hello World");
 
 const person1 = new Person("John Doe", 30);
 person1.greeting();
